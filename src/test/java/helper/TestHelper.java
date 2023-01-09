23b753b225f1f7990e3b272bcf4d37638ecd8164
EHITT <siddharth.sivasubramanian.ext@bayer.com>
2023-01-09 19:23:48 +0530

    Converting Entire Project to Simple TestNG project

--- src/test/java/helper/TestHelper.java
+++ src/test/java/helper/TestHelper.java
@@ -4,4 +4,5 @@ package helper;
  * Created by opantsjoha on 02/07/2017.
  */
 public class TestHelper {
+	
 }
--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -1,5 +1,8 @@
 package tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 import helper.Browser;
 import helper.TestHelper;
 import org.testng.annotations.*;
@@ -9,25 +12,42 @@ import org.testng.annotations.*;
  */
 public class HomePageTests extends TestHelper {
 
-    Browser browser;
-
-    @Parameters({"browserName", "https://github.com/siddharth-tommy/SiddhuGitActions_TestNG/edit/main/src/test/java/tests/HomePageTests.java"})
-    @BeforeClass(groups = {"web"})
-    public void setUp(String browserName, String baseUrl) {
-        browser = new Browser(browserName, baseUrl);
-        browser.navigateToBaseUrl();
-    }
-
-    @Test(groups = {"web"})
-    public void searchTest() throws InterruptedException {
-        browser.HomePage().HeaderSection().setSearchField("Cooking");
-        browser.HomePage().HeaderSection().clickOnSearchButton();
-        // todo: add assert
-    }
-
-    @AfterClass(groups = {"web"})
-    public void tearDown() {
-        browser._driver.quit();
-    }
+//    Browser browser;
+//
+//    @BeforeMethod
+//	@Parameters({"browserName", "https://github.com/siddharth-tommy/SiddhuGitActions_TestNG/edit/main/src/test/java/tests/HomePageTests.java"})
+//    @BeforeClass(groups = {"web"})
+//    public void setUp(String browserName, String baseUrl) {
+//        browser = new Browser(browserName, baseUrl);
+//        browser.navigateToBaseUrl();
+//    }
+//
+//    @Test(groups = {"web"})
+//    public void searchTest() throws InterruptedException {
+//        browser.HomePage().HeaderSection().setSearchField("Cooking");
+//        browser.HomePage().HeaderSection().clickOnSearchButton();
+//        // todo: add assert
+//    }
+//
+//    @AfterMethod
+//	@AfterClass(groups = {"web"})
+//    public void tearDown() {
+//        browser._driver.quit();
+//    }
+	
+	@Test
+	public void testMethod1() {
+		System.out.println("testMethod1 works...");
+	}
+	
+	@Test
+	public void testMethod2() {
+		System.out.println("testMethod2 works...");
+	}
+	
+	@Test
+	public void testMethod3() {
+		System.out.println("testMethod3 works...");
+	}
 
 }
