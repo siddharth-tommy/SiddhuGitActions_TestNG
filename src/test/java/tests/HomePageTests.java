742b331de875876cb0792970db6908bedc35dc10
EHITT <siddharth.sivasubramanian.ext@bayer.com>
2023-01-10 09:30:17 +0530

    Test Commit Jan-10th by Siddharth

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -1,33 +1,47 @@
 package tests;
 
-import helper.Browser;
+import org.testng.annotations.Test;
+
+//import helper.Browser;
 import helper.TestHelper;
-import org.testng.annotations.*;
+//import org.testng.annotations.*;
 
 /**
  * Created by opantsjoha on 02/07/2017.
  */
 public class HomePageTests extends TestHelper {
 
-    Browser browser;
-
-    @Parameters({"browserName", "https://github.com/siddharth-tommy/SiddhuGitActions_TestNG/edit/main/src/test/java/tests/HomePageTests.java"})
-    @BeforeClass(groups = {"web"})
-    public void setUp(String browserName, String baseUrl) {
-        browser = new Browser(browserName, baseUrl);
-        browser.navigateToBaseUrl();
-    }
-
-    @Test(groups = {"web"})
-    public void searchTest() throws InterruptedException {
-        browser.HomePage().HeaderSection().setSearchField("Cooking");
-        browser.HomePage().HeaderSection().clickOnSearchButton();
-        // todo: add assert
-    }
-
-    @AfterClass(groups = {"web"})
-    public void tearDown() {
-        browser._driver.quit();
-    }
-
+//    Browser browser;
+//
+//    @Parameters({"browserName", "https://github.com/siddharth-tommy/SiddhuGitActions_TestNG/edit/main/src/test/java/tests/HomePageTests.java"})
+//    @BeforeClass(groups = {"web"})
+//    public void setUp(String browserName, String baseUrl) {
+//        browser = new Browser(browserName, baseUrl);
+//        browser.navigateToBaseUrl();
+//    }
+//
+//    @Test(groups = {"web"})
+//    public void searchTest() throws InterruptedException {
+//        browser.HomePage().HeaderSection().setSearchField("Cooking");
+//        browser.HomePage().HeaderSection().clickOnSearchButton();
+//        // todo: add assert
+//    }
+//
+//    @AfterClass(groups = {"web"})
+//    public void tearDown() {
+//        browser._driver.quit();
+//    }
+@Test
+public void f1() {
+	System.out.println("test1");
+}
+@Test
+public void f2() {
+	System.out.println("test2");
+}
+@Test
+public void f3() {
+	System.out.println("test3");
+}
+	
 }
